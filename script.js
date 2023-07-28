8ba32573a3013449247fd750421927305fa2fde2
fdemento <francescodemento@gmail.com>
2023-07-28 20:58:21 +0200

    Fix dell'esercizio

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ function eraseTwitter() {
 window.onload = eraseTwitter() //
 
 // Esercizio 2:
+// Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+function showAuthor() {
+
+    let authors = document.querySelectorAll(".blog-post-meta a").innerText
+}
+
+console.log(showAuthor()) //
+
+
+// Esercizio 3:
 // Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
 
 function showAuthor() {
